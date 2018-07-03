64978e0aaf46c59de5bd744e7daf6491e96e45fb
Lafith <mmlafith3@gmail.com>
2018-07-03 12:08:47 +0530

    SAVe arena and menu added
    A menu for loading different scenes is added.Also model of SAVe arena is created.

---
+++ Assets/arena_buttons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class arena_buttons : MonoBehaviour {
+
+    public Rigidbody rb;
+    public Vector3 v;
+
+    //stores AUV's initial position 
+    public void Start()
+    {
+        v = rb.transform.position;
+    }
+
+    //method to launch the main menu
+    public void main_menu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+    //method to reset the transforms of the AUV
+    public void reset_button()
+    {
+
+        rb.transform.position = v;
+        rb.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+}
---
+++ Assets/scene_change.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_change : MonoBehaviour
+{
+   
+  
+    //to load difft scenes:
+    public void toscene0()
+    {
+        SceneManager.LoadScene("scene0");
+    }
+
+    public void toscene1()
+    {
+        SceneManager.LoadScene("SAVe");
+    }
+   /* public void toscene2()
+    {
+        SceneManager.LoadScene("scene2");
+    }*/
+}
